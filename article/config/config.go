b33712f857a868db4a3cd4b68f2c7e448cd8660f
Akinnurun Oluwafemi <femmy4lov@gmail.com>
2020-02-24 13:00:02 +0100

    added config folder

---
+++ article/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// Config contains the configuration of the server and database
+type Config struct {
+	Server Server `json:"server"`
+	DB     DB     `json:"db"`
+}
+
+// Server configuration
+type Server struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+}
+
+// DB configuration
+type DB struct {
+	Driver   string `json:"driver"`
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+}
+
+//  FromFile return a configuration from a given file
+func FromFile(path string) (*Config, error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var cfg *Config
+
+	// Unmarshal data
+	if err := json.Unmarshal(bytes, &cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
